4d7bc48f8883ec4e3ab4fcbf42dafd296737264d
siddhantprateek <siddhantprateek@gmail.com>
2024-12-05 12:56:35 +0530

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +10,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  }
 })
